b75800ec2d3e4de0ff20d6222231a5df7d0a3141
guojw <2315162186@qq.com>
2023-09-13 18:15:57 +0800

    test

--- src/js/math.ts
+++ src/js/math.ts
@@ -1,5 +1,5 @@
-export function add(a: number, b: number): number {
-  return a + b;
+export function add(a: number, b: number, c = 0): number {
+  return a + b + c;
 }
 export function muti(a: number, b: number): number {
   return a * b;
--- tests/math.spec.ts
+++ tests/math.spec.ts
@@ -6,3 +6,7 @@ test('测试add', () => {
 test('测试muti', () => {
   expect(muti(2, 3)).toBe(6);
 });
+
+test('测试number', () => {
+  expect(0.2 + 0.1).toBeCloseTo(0.3);
+});
