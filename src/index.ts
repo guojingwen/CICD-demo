a564dd3d839ffa06cbf58e0b1c3830af744d9944
guojw <2315162186@qq.com>
2023-09-13 22:07:54 +0800

    test

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import createComp from './js/comp';
 import '@/js/resources';
 import { add } from './js/math';
 
-const texts: Array<string> = ['hello', 'WEBPACK'];
+const texts: Array<string> = ['hello', 'CICD'];
 const comp = createComp('h2', { class: 'content' }, texts.at(-1));
 document.body.appendChild(comp);
 
