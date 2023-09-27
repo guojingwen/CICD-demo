f349a05af87f921b03c2aa9ded99a4bf2b1eb616
guojw <2315162186@qq.com>
2023-09-27 19:56:57 +0800

    feat: test

--- src/js/math.ts
+++ src/js/math.ts
@@ -2,5 +2,5 @@ export function add(...args: number[]): number {
   return args.reduce((sum, it) => sum + it, 0);
 }
 export function muti(...args: number[]): number {
-  return args.reduce((sum, it) => sum * it, 1);
+  return args.reduce((sum, it) => sum * it, 1.0);
 }
