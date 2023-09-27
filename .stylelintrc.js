c2b07315702a170d9cf259feb49fbc3f3eb199c1
guojw <2315162186@qq.com>
2023-09-27 19:30:07 +0800

    chore: test2

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,11 +1,14 @@
 module.exports = {
-  extends: [
-    'stylelint-config-standard',
-    'stylelint-config-standard-scss',
-    'stylelint-config-prettier',
+  "extends": [
+    "stylelint-config-standard",
+    "stylelint-config-standard-scss"
   ],
-  plugins: [],
-  rules: {
-    'string-quotes': 'double',
-  },
+  "rules": {
+    "declaration-empty-line-before": "never",
+    "unit-allowed-list": [
+      "em",
+      "rem"
+    ],
+    "selector-max-id": 1
+  }
 };
